b5f2143912fe4e2d0bb1b6dc6776ffdf525e48a9
DonQueso89 <kg.v.ekeren@gmail.com>
2021-02-06 17:09:08 +0100

    ajax mustachification. move css to static. revamp index a bit

--- static/index.js
+++ static/index.js
@@ -1,10 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-    function setPreviewImg(e) {
-        const output = document.getElementById("imgpreview")
-        output.src = URL.createObjectURL(e.target.files[0]);
-        output.onload = function() {
-            URL.revokeObjectURL(output.src)
-        }
+  function setPreviewImg(e) {
+    const output = document.getElementById("imgpreview");
+    output.src = URL.createObjectURL(e.target.files[0]);
+    output.onload = function () {
+      URL.revokeObjectURL(output.src);
+    };
+  }
+  document.getElementById("img-inp").onchange = setPreviewImg;
+
+  function validateResponse(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
     }
-    document.getElementById("imgInp").onchange = setPreviewImg
-})
+    return response;
+  }
+
+  function ajaxSubmission(e) {
+    e.preventDefault();
+    const form = e.target;
+    const url = form.action;
+
+    fetch(url, { method: "POST", body: new FormData(form) })
+      .then(validateResponse)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const output = document.getElementById("imgpreview");
+        output.src = URL.createObjectURL(blob);
+        output.onload = function () {
+          URL.revokeObjectURL(output.src);
+        };
+      });
+  }
+  document.getElementById("file-form").onsubmit = ajaxSubmission;
+});
